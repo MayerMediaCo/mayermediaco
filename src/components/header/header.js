06f5e8b2c0d303682e0859927a56ac3708e66863
mayermediaco <mayermediaco@gmail.com>
2019-02-26 21:25:32 -0600

    Minor style change

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,16 +2,13 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const Header = () => (
-  <section className='hero header is-medium'>
-    <div className=' container hero-head'></div>
-    <div className='hero-body'>
-      <div className='container has-text-centered'>
-        <h1 className='title'>
-          Mayer Media Co.
-        </h1>
-        <h3 className='subtitle'>
-          <Link to='services'>Design | Development | Marketing
-          </Link>
+  <section className="hero header is-medium">
+    <div className=" container hero-head" />
+    <div className="hero-body">
+      <div className="container has-text-centered">
+        <h1 className="title">Mayer Media Co.</h1>
+        <h3 className="subtitle">
+          <Link to="services">Design | Development | Marketing</Link>
         </h3>
       </div>
     </div>
