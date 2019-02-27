841badb7da987f172bf879e209ab78b58fa66caa
mayermediaco <mayermediaco@gmail.com>
2019-02-26 20:21:42 -0600

    Added ESLint

--- src/components/buttons/get-started-now.js
+++ src/components/buttons/get-started-now.js
@@ -2,8 +2,9 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const GetStartedNowButton = () => (
-  <div className='has-text-centered button-animation'>
-    <Link to='/start-your-project' className='button is-medium is-rounded'>Get Started Now
+  <div className="has-text-centered button-animation">
+    <Link to="/start-your-project" className="button is-medium is-rounded">
+      Get Started Now
     </Link>
   </div>
 )
