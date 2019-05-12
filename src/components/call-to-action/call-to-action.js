9674491c16128c60145d8da71a5ce164a03d416d
Danny Mayer <mayermediaco@gmail.com>
2019-05-11 20:33:46 -0500

    cssnav

--- src/components/call-to-action/call-to-action.js
+++ src/components/call-to-action/call-to-action.js
@@ -2,14 +2,16 @@ import React from 'react'
 import EmailCaptureForm from '../email-capture-form/email-capture-form'
 
 const CallToAction = () => (
-  <div id='call-to-action'>
-    <div className="container">
-      <div className="columns">
-        <div className="column">
-          <h3>Want weekly information to help push your brand forward?</h3>
-        </div>
-        <div className="column">
-          <EmailCaptureForm/>
+  <div id="call-to-action">
+    <div className="section">
+      <div className="container">
+        <div className="columns">
+          <div className="column">
+            <h3>Want weekly information to help push your brand forward?</h3>
+          </div>
+          <div className="column">
+            <EmailCaptureForm />
+          </div>
         </div>
       </div>
     </div>
